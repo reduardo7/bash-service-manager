0d82aadd2c4dfca31280a8e3f2e7fd8f793d03f7
Eduardo Cuomo <reduardo7@gmail.com>
2017-06-01 20:16:41 -0300

    Documentation

--- README.md
+++ README.md
@@ -2,3 +2,123 @@
 Bash Script's for Service Manager
 
 Create your custom service for development.
+
+## Usage ##
+
+1. Create your _service script_.
+2. Copy `services.sh` content or import it at the beginning.
+3. Call `serviceMenu` function with next format: `serviceMenu ACTION SERVICE_NAME COMMAND [WORK_DIR]`
+4. Make your new _service script_ executable: `chmod a+x my-service-script`
+5. Use it!
+
+## servicesMenu function ##
+
+Just call only this function to make everything work!
+
+### Arguments ###
+
+#### 1: ACTION ####
+
+This is the action to execute. Please see _Actions_ section below for more information.
+
+If it is an invalid action or emtpy action, you can see the _help_.
+
+#### 2: SSERVICE_NAME ####
+
+This is the user friendly _Service Name_.
+
+#### 3: COMMAND ####
+
+This is the command (or commands) that you must execute to start _your service_.
+
+#### 4: WORK_DIR ####
+
+**This is optional**. The working directory is set, where it must be located to execute the _COMMAND_.
+
+## Actions ##
+
+If it is an invalid action or emtpy action, you can see the _help_.
+
+* `start`: Start the _service_.
+* `stop`: Stop the _service_.
+* `restart`: Restart the _service_. If the _service_ is running, first call _stop_ then call _start_.
+* `status`: Get _service_ status.
+* `tail`: See all _service_ output.
+* `tail-log`: See _service_ _std_ output.
+* `tail-error`: See _service_ _err_ output.
+* `run`: Execute _service_ _command_ and exit.
+* `debug`: Run _start service_ action and wait for user cancelation.
+
+## Examples ##
+
+### MongoDB Service ###
+
+**mongo** file:
+
+```bash
+#!/usr/bin/env bash
+
+# Import or paste "services.sh"
+. ./services.sh
+
+action="$1"
+serviceName="mongodb"
+command="sudo rm -f /data/db/mongod.lock >/dev/null 2>&1 ; sudo mongod"
+
+serviceMenu "$action" "$serviceName" "$command"
+```
+
+In console:
+
+```bash
+$ mongo status
+$ mongo restart
+```
+
+### Custom Service ###
+
+**my-service** file:
+
+```bash
+#!/usr/bin/env bash
+
+# Import or paste "services.sh"
+. ./services.sh
+
+action="$1"
+serviceName="my-service"
+command="bash my-service-script.sh"
+workDir="/opt/my-service"
+
+serviceMenu "$action" "$serviceName" "$command" "$workDir"
+```
+
+### Multiple Services ###
+
+**my-services** file:
+
+```bash
+#!/usr/bin/env bash
+
+# Import or paste "services.sh"
+. ./services.sh
+
+# Mong
+mongo() {
+	serviceMenu "$1" "mongodb" "sudo rm -f /data/db/mongod.lock >/dev/null 2>&1 ; sudo mongod"
+}
+
+# Custom Service
+custom() {
+	serviceMenu "$1" "my-script" "bash my-script.sh" "/home/user/my-project"
+}
+
+$1 $2
+```
+
+In console:
+
+```bash
+$ my-services mongo status
+$ my-services custom restart
+```
--- services.sh
+++ services.sh
@@ -1,176 +1,165 @@
 #!/usr/bin/env bash
 
 serviceStatus() {
-	local serviceName="$1" # Service Name
-	local pidFile="/tmp/proccess-${serviceName}.pid"
+  local serviceName="$1" # Service Name
+  local pidFile="/tmp/proccess-${serviceName}.pid"
 
-	if [ -f "$pidFile" ] && [ ! -z "$(cat "$pidFile")" ]; then
-		local err=1
-		for p in $(cat "$pidFile"); do
-			if kill -0 $p >/dev/null 2>&1
-				then
-					e "Proccess with PID $p is running"
-					err=0
-				else
-					e "Proccess with PID $p is not running"
-				fi
-		done
-		return $err
-	else
-		e "Warning: PID file (${pidFile}) not exists or is empty"
-		return 2
-	fi
+  if [ -f "$pidFile" ] && [ ! -z "$(cat "$pidFile")" ]; then
+    local err=1
+    for p in $(cat "$pidFile"); do
+      if kill -0 $p >/dev/null 2>&1
+        then
+          e "Proccess with PID $p is running"
+          err=0
+        else
+          e "Proccess with PID $p is not running"
+        fi
+    done
+    return $err
+  else
+    e "Warning: PID file (${pidFile}) not exists or is empty"
+    return 2
+  fi
 }
 
 serviceStart() {
-	local serviceName="$1" # Service Name
-	local c="$2" # Command
-	local w="$3" # Workdir
-	local logFilePath="$PROJECTS_DIR"
-	local pidFile="/tmp/proccess-${serviceName}.pid"
-	local logFile="${logFilePath}/${serviceName}.log"
-	local logErrorFile="${logFilePath}/${serviceName}.error.log"
+  local serviceName="$1" # Service Name
+  local c="$2" # Command
+  local w="$3" # Workdir
+  local logFilePath="$PROJECTS_DIR"
+  local pidFile="/tmp/proccess-${serviceName}.pid"
+  local logFile="${logFilePath}/${serviceName}.log"
+  local logErrorFile="${logFilePath}/${serviceName}.error.log"
 
-	if serviceStatus "$1" >/dev/null 2>&1
-		then
-			e "Service ${serviceName} already running with PID $(cat "$pidFile")"
-			return 0
-		fi
+  if serviceStatus "$1" >/dev/null 2>&1
+    then
+      e "Service ${serviceName} already running with PID $(cat "$pidFile")"
+      return 0
+    fi
 
-	e "Starting ${serviceName} service..."
-	touch "$logFile" >/dev/null 2>&1
-	chmod a+rw "$logFile" >/dev/null 2>&1
-	touch "$logErrorFile" >/dev/null 2>&1
-	chmod a+rw "$logErrorFile" >/dev/null 2>&1
-	touch "$pidFile" >/dev/null 2>&1
-	chmod a+rw "$pidFile" >/dev/null 2>&1
+  e "Starting ${serviceName} service..."
+  touch "$logFile" >/dev/null 2>&1
+  chmod a+rw "$logFile" >/dev/null 2>&1
+  touch "$logErrorFile" >/dev/null 2>&1
+  chmod a+rw "$logErrorFile" >/dev/null 2>&1
+  touch "$pidFile" >/dev/null 2>&1
+  chmod a+rw "$pidFile" >/dev/null 2>&1
 
-	[ ! -z "$w" ] && cd "$w"
-	bash -i -c "$c >>\"$logFile\" 2>>\"$logErrorFile\" & echo \$! >>\"$pidFile\""
+  [ ! -z "$w" ] && cd "$w"
+  bash -i -c "$c >>\"$logFile\" 2>>\"$logErrorFile\" & echo \$! >>\"$pidFile\""
 
-	serviceStatus "$1" >/dev/null 2>&1
-	return $?
+  serviceStatus "$1" >/dev/null 2>&1
+  return $?
 }
 
 serviceStop() {
-	local serviceName="$1" # Service Name
-	local pidFile="/tmp/proccess-${serviceName}.pid"
+  local serviceName="$1" # Service Name
+  local pidFile="/tmp/proccess-${serviceName}.pid"
 
-	if [ -f "$pidFile" ] && [ ! -z "$(cat "$pidFile")" ]; then
-		e "Stopping ${serviceName}..."
-		for p in $(cat "$pidFile"); do
-			if kill -0 $p >/dev/null 2>&1
-				then
-					kill $p
-					sleep 2
-					if kill -0 $p >/dev/null 2>&1
-						then
-							kill -9 $p
-							sleep 2
-							if kill -0 $p >/dev/null 2>&1
-								then
-									e "Exec: sudo kill -9 $p"
-									sudo kill -9 $p
-								fi
-						fi
-				fi
-		done
-		rm -f "$pidFile"
-		touch "$pidFile" >/dev/null 2>&1
-		chmod a+rw "$pidFile" >/dev/null 2>&1
-	else
-		e "Warning: PID file (${pidFile}) not exists or is empty"
-	fi
+  if [ -f "$pidFile" ] && [ ! -z "$(cat "$pidFile")" ]; then
+    e "Stopping ${serviceName}..."
+    for p in $(cat "$pidFile"); do
+      if kill -0 $p >/dev/null 2>&1
+        then
+          kill $p
+          sleep 2
+          if kill -0 $p >/dev/null 2>&1
+            then
+              kill -9 $p
+              sleep 2
+              if kill -0 $p >/dev/null 2>&1
+                then
+                  e "Exec: sudo kill -9 $p"
+                  sudo kill -9 $p
+                fi
+            fi
+        fi
+    done
+    rm -f "$pidFile"
+    touch "$pidFile" >/dev/null 2>&1
+    chmod a+rw "$pidFile" >/dev/null 2>&1
+  else
+    e "Warning: PID file (${pidFile}) not exists or is empty"
+  fi
 }
 
 serviceRestart() {
-	local serviceName="$1" # Service Name
-	local c="$2" # Command
-	local w="$3" # Workdir
+  local serviceName="$1" # Service Name
+  local c="$2" # Command
+  local w="$3" # Workdir
 
-	serviceStop "$serviceName"
-	serviceStart "$serviceName" "$c" "$w"
+  serviceStop "$serviceName"
+  serviceStart "$serviceName" "$c" "$w"
 }
 
 serviceTail() {
-	local serviceName="$1" # Service Name
-	local type="$2"
-	local logFilePath="$PROJECTS_DIR"
-	local logFile="${logFilePath}/${serviceName}.log"
-	local logErrorFile="${logFilePath}/${serviceName}.error.log"
+  local serviceName="$1" # Service Name
+  local type="$2"
+  local logFilePath="$PROJECTS_DIR"
+  local logFile="${logFilePath}/${serviceName}.log"
+  local logErrorFile="${logFilePath}/${serviceName}.error.log"
 
-	case "$type" in
-		log)
-			tail -f "$logFile"
-			exit 0
-			;;
-		error)
-			tail -f "$logErrorFile"
-			exit 0
-			;;
-		all)
-			tail -f "$logFile" "$logErrorFile"
-			exit 0
-			;;
-		*)
-			e "Actions: [log|error]"
-			exit 1
-			;;
-	esac
+  case "$type" in
+    log)
+      tail -f "$logFile"
+      exit 0
+      ;;
+    error)
+      tail -f "$logErrorFile"
+      exit 0
+      ;;
+    all)
+      tail -f "$logFile" "$logErrorFile"
+      exit 0
+      ;;
+    *)
+      e "Actions: [log|error]"
+      exit 1
+      ;;
+  esac
 }
 
 serviceMenu() {
-	local action="$1"
-	local serviceName="$2"
-	local c="$3"
-	local w="$4"
+  local action="$1"
+  local serviceName="$2"
+  local c="$3"
+  local w="$4"
 
-	case "$action" in
-		start)
-			serviceStart "$serviceName" "$c" "$w"
-			;;
-		stop)
-			serviceStop "$serviceName"
-			;;
-		restart)
-			serviceRestart "$serviceName" "$c" "$w"
-			;;
-		status)
-			serviceStatus "$serviceName"
-			;;
-		debug)
-			serviceStop "$serviceName"
-			e "Debugging ${serviceName}..."
-			[ ! -z "$w" ] && cd "$w"
-			bash -i -c "$c"
-			;;
-		tail)
-			serviceTail "$serviceName" "all"
-			;;
-		tail-log)
-			serviceTail "$serviceName" "log"
-			;;
-		tail-error)
-			serviceTail "$serviceName" "error"
-			;;
-		*)
-			e "Actions: [start|stop|restart|status|debug|tail(-[log|error])]"
-			exit 1
-			;;
-	esac
+  case "$action" in
+    start)
+      serviceStart "$serviceName" "$c" "$w"
+      ;;
+    stop)
+      serviceStop "$serviceName"
+      ;;
+    restart)
+      serviceRestart "$serviceName" "$c" "$w"
+      ;;
+    status)
+      serviceStatus "$serviceName"
+      ;;
+    run)
+      [ ! -z "$w" ] && cd "$w"
+      bash -i -c "$c"
+      ;;
+    debug)
+      serviceStop "$serviceName"
+      e "Debugging ${serviceName}..."
+      [ ! -z "$w" ] && cd "$w"
+      bash -i -c "$c"
+      ;;
+    tail)
+      serviceTail "$serviceName" "all"
+      ;;
+    tail-log)
+      serviceTail "$serviceName" "log"
+      ;;
+    tail-error)
+      serviceTail "$serviceName" "error"
+      ;;
+    *)
+      e "Actions: [start|stop|restart|status|run|debug|tail(-[log|error])]"
+      exit 1
+      ;;
+  esac
 }
-
-
-
-# Example: MongoDB Service
-__mongo() {
-	serviceMenu "$1" "mongodb" "sudo rm -f /data/db/mongod.lock >/dev/null 2>&1 ; sudo mongod"
-}
-__mongo start
-__mongo stop
-
-# Example: Custom Service
-__custom() {
-	serviceMenu "$1" "my-script" "bash my-script.sh" "/home/user/my-project"
-}
-__custom status
